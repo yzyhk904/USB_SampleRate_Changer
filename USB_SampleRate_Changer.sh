959d435d117637a28c26f29e232ebbee244e7570
yzyhk904 <86816519+yzyhk904@users.noreply.github.com>
2021-07-14 17:26:14 +0900

    Add files via upload
    (USB) hardware offload support (currently experimental) added

--- USB_SampleRate_Changer.sh
+++ USB_SampleRate_Changer.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
 #
-# Version: 1.11
+# Version: 1.2.0
 #     by zyhk
 
 MYDIR=${0%/*}
@@ -15,18 +15,43 @@ MYDIR=${0%/*}
   fi
  # End
  
- # Help message
-   if [ $# -gt 0 ]; then
+# Help message
+  offloadMode="false"
+  resetMode="false"
+  if [ $# -gt 0 ]; then
      case "$1" in
-       "-h" | "--help" )
-         echo "${0##*/} [[44k|48k|88k|96k|176k|192k|353k|384k|706k|768k] [[16|24|32]]]" 1>&2
-         echo "  Note: ${0##*/} requires to unlock the USB audio class driver's limitation (96kHz lock)" 1>&2
-         echo "           if you specify greater than 96kHz" 1>&2
+       "-o" | "--offload" )
+         offloadMode="true"
+         shift
+         ;;
+       "-r" | "--reset" )
+         resetMode="true"
+         shift
+         ;;
+       "-h" | "--help" | -* )
+         echo "Usage: ${0##*/} [--reset][--offload] [[44k|48k|88k|96k|176k|192k|353k|384k|706k|768k] [[16|24|32]]]" 1>&2
+         echo "  Note: ${0##*/} requires to unlock the USB audio class driver's limitation (upto 96kHz lock or 192kHz offload lock)" 1>&2
+         echo "           if you specify greater than 96kHz or 192kHz (in case of offload)" 1>&2
          exit 0
          ;;
      esac
-   fi
- # End of help message
+  fi
+# End of help message
+ 
+# Reset
+  if "$resetMode"; then
+    for i in "/vendor/etc/audio_policy_configuration.xml" "/vendor/etc/audio/audio_policy_configuration.xml" \
+  	  "/vendor/etc/usb_audio_policy_configuration.xml" "/system/etc/usb_audio_policy_configuration.xml"; do
+       if [ -r "$i" ]; then
+          umount "$i" 1>"/dev/null" 2>&1
+       fi
+    done
+    if [ "`getprop init.svc.audioserver`" = "running" ]; then
+      setprop ctl.restart audioserver
+    fi
+    exit 0
+  fi
+# Reset End
  
 # Set parameters
   sRate="44100"
@@ -85,8 +110,10 @@ MYDIR=${0%/*}
       ;;
   esac
 
-  if [ $sRate -gt 96000 ]; then
-    echo "    Warning: ${0##*/} requires to unlock the USB audio class driver's limitation (96kHz lock)" 1>&2
+  if [ "$offloadMode" = "false"  -a  $sRate -gt 96000 ]; then
+    echo "    Warning: ${0##*/} requires to unlock the USB audio class driver's limitation (upto 96kHz lock)" 1>&2
+  elif [ "$offloadMode" = "true"  -a  $sRate -gt 192000 ]; then
+    echo "    Warning: ${0##*/} requires to unlock the USB audio hardware offload driver's limitation (upto 192kHz lock)" 1>&2
   fi
 
   aFormat="AUDIO_FORMAT_PCM_32_BIT"
@@ -107,9 +134,15 @@ MYDIR=${0%/*}
   esac
 # End of argument normalization
 
-# Overlay a generated usb audio policy configuration file on "/vendor/etc/usb_audio_policy_configuration.xml"
-  template="$MYDIR/usb_conf_template.xml"
+# Overlay a generated {usb} audio policy configuration file on "/vendor/etc/{usb_}audio_policy_configuration.xml"
   genfile="/data/local/tmp/usb_conf_generated.xml"
+  if "$offloadMode"; then
+    template="$MYDIR/usb_conf_offload_template.xml"
+    overlayTarget="/vendor/etc/audio_policy_configuration.xml"
+  else
+    template="$MYDIR/usb_conf_template.xml"
+    overlayTarget="/vendor/etc/usb_audio_policy_configuration.xml"
+  fi
 
   if [ -r "$template" ]; then
     if [ -e "$genfile" ]; then
@@ -118,15 +151,22 @@ MYDIR=${0%/*}
     sed -e "s/%SAMPLING_RATE%/$sRate/g" -e "s/%AUDIO_FORMAT%/$aFormat/g" "$template" >"$genfile"
     if [ $? -eq 0 ]; then
       chmod 644 "$genfile"
-      if [ -r "/vendor/etc/usb_audio_policy_configuration.xml" ]; then
-        umount "/vendor/etc/usb_audio_policy_configuration.xml" 1>"/dev/null" 2>&1
-        mount -o bind "$genfile" "/vendor/etc/usb_audio_policy_configuration.xml"
+      if [ -r "$overlayTarget" ]; then
+        umount "$overlayTarget" 1>"/dev/null" 2>&1
+        mount -o bind "$genfile" "$overlayTarget"
         if [ $? -gt 0 ]; then
           echo "overlaying a generated USB configuration XML file failed!" 1>&2 
           exit 1
         fi
+      else
+        echo "overlaying target (\"$overlayTarget\") doesn't exist!" 1>&2 
+        exit 1
       fi
-      if [ -r "/system/etc/usb_audio_policy_configuration.xml" ]; then
+      if [ "$overlayTarget" = "/vendor/etc/audio_policy_configuration.xml"  -a  -r "/vendor/etc/audio/audio_policy_configuration.xml" ]; then
+        umount "/vendor/etc/audio/audio_policy_configuration.xml" 1>"/dev/null" 2>&1
+        mount -o bind "$genfile" "/vendor/etc/audio/audio_policy_configuration.xml"
+      fi
+      if [ "$overlayTarget" = "/vendor/etc/usb_audio_policy_configuration.xml"  -a  -r "/system/etc/usb_audio_policy_configuration.xml" ]; then
         umount "/system/etc/usb_audio_policy_configuration.xml" 1>"/dev/null" 2>&1
         mount -o bind "$genfile" "/system/etc/usb_audio_policy_configuration.xml"
       fi
--- extras/alsa-hw-params.sh
+++ extras/alsa-hw-params.sh
@@ -1,10 +1,32 @@
 #!/system/bin/sh
 
-if [ ! "`cat /proc/asound/card1/pcm0p/sub0/hw_params`" = "closed" ];then
-  echo "DAC Connection Status:"
-  cat '/proc/asound/card1/pcm0p/sub0/hw_params'
+rst="closed"
+if [ -r "/proc/asound/card1/pcm0p/sub0/hw_params" ]; then
+#  For non-hardware offload use
+  rst="`cat /proc/asound/card1/pcm0p/sub0/hw_params`"
+  if [ ! "$rst" = "closed" ];then
+    echo "DAC Connection Status:"
+    echo "$rst"
+  fi
 fi
-if [ ! "`cat /proc/asound/card1/id`" = "closed" ];then
-  echo "\nDAC Hardware Profile:"
-  cat '/proc/asound/card1/stream0'
+if [ "$rst" = "closed" ]; then
+#  For USB hardware offload use
+  for i in `seq 1 60`; do
+    if [ -r "/proc/asound/card0/pcm${i}p/sub0/hw_params" ]; then
+      rst="`cat /proc/asound/card0/pcm${i}p/sub0/hw_params`"
+      if [ ! "$rst" = "closed" ];then
+          echo "DAC Connection Status:"
+          echo "$rst"
+          break
+      fi
+    fi
+  done
 fi
+
+if [ -r "/proc/asound/card1/id" ]; then
+  if [ ! "`cat /proc/asound/card1/id`" = "closed"  -a  -r "/proc/asound/card1/stream0" ];then
+    echo "\nDAC Hardware Profile:"
+    cat '/proc/asound/card1/stream0'
+  fi
+fi
+
