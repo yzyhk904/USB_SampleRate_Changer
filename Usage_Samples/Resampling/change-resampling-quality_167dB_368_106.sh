3084fb112f9f726c5bd2f1927372fe79d41f9e00
yzyhk904 <86816519+yzyhk904@users.noreply.github.com>
2024-05-19 15:33:13 +0900

    Delete Usage_Samples/Resampling/change-resampling-quality_167dB_368_106.sh

--- Usage_Samples/Resampling/change-resampling-quality_167dB_368_106.sh
+++
@@ -1,9 +0,0 @@
-#!/system/bin/sh
-#
-# General purpos re-sampling filter at a mastering quality (for low performance devices)
-#
-#   Characteristics: slight in background aliasing noise, very slight in pre-echo and ringing, and medium in intermodulation
-#
-
-MODDIR=${0%/*/*/*}
-su -c "/system/bin/sh ${MODDIR}/extras/change-resampling-quality.sh --bypass --cheat 167 368 106"
