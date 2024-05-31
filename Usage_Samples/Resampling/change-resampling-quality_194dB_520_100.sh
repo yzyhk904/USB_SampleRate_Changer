4bcae0152daa0b9125833eb63287ba54fcd14587
yzyhk904 <86816519+yzyhk904@users.noreply.github.com>
2024-05-31 22:12:34 +0900

    Add files via upload

--- Usage_Samples/Resampling/change-resampling-quality_194dB_520_100.sh
+++ Usage_Samples/Resampling/change-resampling-quality_194dB_520_100.sh
@@ -2,7 +2,7 @@
 #
 # The ideal re-sampling (bit-perfect) only when 1:1 ratio frequency re-sampling and 32 bit depth
 #
-#   Characteristics (except 1:1 re-sampling) : almost none in background aliasing noise, light in pre-echo and ringing, and slight in intermodulation
+#   Characteristics (except 1:1 re-sampling) : almost none in background aliasing noise, light in pre-echo and ringing, and light in intermodulation
 #
 
 MODDIR=${0%/*/*/*}
