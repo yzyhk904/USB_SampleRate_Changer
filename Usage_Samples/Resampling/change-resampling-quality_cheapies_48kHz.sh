27babe29d83e4cccb4f2ff0803e4d5a02ce81c4b
yzyhk904 <86816519+yzyhk904@users.noreply.github.com>
2024-07-28 15:06:23 +0900

    Add files via upload
    * Changed the default Bluetooth sample rate of "safer" templates from 44.1 kHz to 48 kHz for Am@zon music SD (Opus 48 kHz & 192 kbps vbr stereo) tracks
    * Adjusted jitter optimizations and others for YTM's format change from AAC (141; 44.1 kHz & 256 kbps cbr stereo) to Opus (774; 48 kHz & 256 kbps vbr stereo)

---
+++ Usage_Samples/Resampling/change-resampling-quality_cheapies_48kHz.sh
@@ -0,0 +1,14 @@
+#!/system/bin/sh
+#
+# For LDAC earphones and DAC's under $30 having large non-linear distortion when replaying 48 kHz & 16 and 24 bits tracks (possibly 44.1 kHz ones)
+#
+#  If you feel your LDAC earphones or internal speakers wouldn't become to sound good at all, 
+#  try replacing "85" (below) with "86" or "87" for appropriately cutting off ultrasonic noise causing intermodulation.
+#
+#   Characteristics: almost none in background aliasing noise, almost none in pre-echo and ringing, and very slight in intermodulation
+#
+# Try to use this for investigating differences of 179dB_408_99 and "cheapies_48kHz" after setting from 48kHz to 768kHz (8x) or 384kHz (4x) in 32bit depth up-sampling
+#
+
+MODDIR=${0%/*/*/*}
+su -c "/system/bin/sh ${MODDIR}/extras/change-resampling-quality.sh 194 520 85"
