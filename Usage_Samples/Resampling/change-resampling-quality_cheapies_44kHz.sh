3d1f6f1dda350f05bfa6a9f3e39530cb59ea4f9c
yzyhk904 <86816519+yzyhk904@users.noreply.github.com>
2024-07-28 15:10:06 +0900

    Update change-resampling-quality_cheapies_44kHz.sh

--- Usage_Samples/Resampling/change-resampling-quality_cheapies_44kHz.sh
+++ Usage_Samples/Resampling/change-resampling-quality_cheapies_44kHz.sh
@@ -7,7 +7,7 @@
 #
 #   Characteristics: almost none in background aliasing noise, almost none in pre-echo and ringing, and very slight in intermodulation
 #
-# Try to use this for investigating differences of 179dB_408_99 and "cheapies_44k" after setting from 44.1kHz to 706kHz (16x) or 354kHz (8x) in 32bit depth up-sampling
+# Try to use this for investigating differences of 179dB_408_99 and "cheapies_44kHz" after setting from 44.1kHz to 706kHz (16x) or 354kHz (8x) in 32bit depth up-sampling
 #
 
 MODDIR=${0%/*/*/*}
