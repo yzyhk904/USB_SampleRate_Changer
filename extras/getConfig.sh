350a33b8013c8fbe1562589eed99a3407fa3fb6b
yzyhk904 <86816519+yzyhk904@users.noreply.github.com>
2025-06-15 23:10:19 +0900

    Add files via upload

--- extras/getConfig.sh
+++ extras/getConfig.sh
@@ -21,7 +21,35 @@ if [ $# -gt 0 ];then
 fi
 
 dumpsys media.audio_policy | sed -e '/^Supported/,/^$/d' -e '/^ Outputs/,$d' \
-    | awk -v allFlag=$allFlag ' 
+    | awk -v allFlag=$allFlag '
+    function getDRCswitch(fname)
+    {
+        v=0
+        if (system("test -r " fname) > 0)
+            return(-1)
+        for (;;) {
+            r=(getline x < fname)
+            if (r < 0)
+                retrun(-1)
+            else if ( r == 0) {
+                v=0
+                break
+            }
+            else {
+                if (x ~ /speaker_drc_enabled[:space:]*=[:space:]*\"true\"/) {
+                    v=1
+                    break
+                }
+                else if (x ~ /speaker_drc_enabled[:space:]*=[:space:]*\"false\"/) {
+                    v=0
+                    break
+                }
+            }
+        }
+        close(fname)
+        return(v)
+    }
+
     allFlag==1 {
         print
     }
@@ -32,6 +60,11 @@ dumpsys media.audio_policy | sed -e '/^Supported/,/^$/d' -e '/^ Outputs/,$d' \
     allFlag!=1 &&
     /^ Config source: / {
         print  " Config source: \"" $3 "\""
+        v=getDRCswitch($3)
+        if (v == 1)
+            print  " DRC switch: on"
+        else if (v == 0)
+            print  " DRC switch: off"
     }
     allFlag!=1 &&
     /^  [1-9][0-9]*\. Handle:/ {
